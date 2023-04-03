4edf61796a634a93d78a08bf94d7bbdf61991354
Kubiera <zbuchananmull@gmail.com>
2023-04-03 16:54:44 -0400

    Initialized Android Studio Project

---
+++ app/src/main/java/com/cs388group6/packer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cs388group6.packer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
