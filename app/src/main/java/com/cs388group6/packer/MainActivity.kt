918e2168d0a48f7dfa57e2f11b9a43dfa5f91b90
Shane Arcaro <smarcaro@gmail.com>
2023-04-10 18:28:26 -0400

    MainActivity push

--- app/src/main/java/com/cs388group6/packer/MainActivity.kt
+++ app/src/main/java/com/cs388group6/packer/MainActivity.kt
@@ -2,10 +2,33 @@ package com.cs388group6.packer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+
+        Log.d("MAINACTIVITY", currentUser.toString())
+
+        if (currentUser != null) {
+            Log.d("MAINACTIVITY", "USER IS LOGGED IN")
+        }
+        else {
+            Log.d("MAINACTIVITY", "NO USER")
+        }
     }
 }
